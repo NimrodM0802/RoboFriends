c30b70de5a916944a6b471ceab08a1307965d873
NimrodM0802 <nimrodmalnegro@gmail.com>
2019-10-23 15:03:15 +0800

    Updates

---
+++ precache-manifest.8124cf58894f92e790d6b3e603d81f67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8203d56017965c4d7a749a7793ee0ba6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "797b5010ba291bc81847",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "29e519c1369ecfe8473e",
+    "url": "/robofriends/static/css/main.899925cf.chunk.css"
+  },
+  {
+    "revision": "797b5010ba291bc81847",
+    "url": "/robofriends/static/js/2.a8322b46.chunk.js"
+  },
+  {
+    "revision": "29e519c1369ecfe8473e",
+    "url": "/robofriends/static/js/main.64216f55.chunk.js"
+  },
+  {
+    "revision": "cb519fe66fcbb61e7a5c",
+    "url": "/robofriends/static/js/runtime-main.0560b89d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
